d1c24bf084edcc552bbe5607275a58fcab40c65e
alessandro-barletta <alessandro.barletta93@gmail.com>
2026-04-14 20:53:58 +0000

    Deploying to gh-pages from @ alessandro-barletta/alessandro-barletta.github.io@b083cc242db31e3e5f6ba33db9b5f409d256ecfa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,16 +35,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
