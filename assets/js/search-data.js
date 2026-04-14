efb6a04c0aef8d22dbd387ea365ee53a22c48e91
alessandro-barletta <alessandro.barletta93@gmail.com>
2026-04-14 20:56:12 +0000

    Deploying to gh-pages from @ alessandro-barletta/alessandro-barletta.github.io@08366625c5ca8febbeef7933bd8e2f86776bca8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,11 +35,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
         id: 'social-rss',
         title: 'RSS Feed',
